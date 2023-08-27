00be0071938ef6208db47fc3a6bdc1263b071708
Hakeem <12151899q@gmail.com>
2023-08-26 17:48:31 -0700

    WIP

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,32 @@
+const { Tech, Matchup, User } = require('../models');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+
+    },
+
+  },
+  Mutation: {
+    login: async (parent, args) => {
+
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      if (!user) {
+        return
+      }
+      const token = signToken(user);
+      return { token, user };
+
+    },
+    saveBook: async (parent, { _id, techNum }) => {
+
+    },
+    removeBook: async (parent, { _id, techNum }) => {
+
+    },
+  },
+};
+
+module.exports = resolvers;
